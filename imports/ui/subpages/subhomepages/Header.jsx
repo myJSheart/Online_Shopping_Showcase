01c1003ca9e5a41a6eb2b00ebce6ea9165ed10f8
Chenxu Zhao <zhaochenxu001@gmail.com>
2017-01-21 12:34:34 +1100

    Add my account logo in Header

--- imports/ui/subpages/subhomepages/Header.jsx
+++ imports/ui/subpages/subhomepages/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { TAPi18n } from 'meteor/tap:i18n';
 import './style.css';
 
 class Header extends Component {
@@ -8,15 +9,17 @@ class Header extends Component {
         <header style={{ background: '#BADDD6' }} className="mdl-layout__header">
           <div className="mdl-layout__header-row">
             <span className="mdl-layout-title">
-              <img src="logo.png" alt="Green World" width="80px" height="80px" href="/homepage/" />
+              <img src="logo.png" alt={TAPi18n.__('title.websitename')} width="80px" height="80px" href="/homepage/" />
             </span>
             <div className="mdl-layout-spacer" />
             <nav className="mdl-navigation mdl-layout--large-screen-only">
-              <a className="mdl-navigation__link" href="">Link</a>
-              <a className="mdl-navigation__link" href="">Link</a>
-              <a className="mdl-navigation__link" href="">Link</a>
-              <a className="mdl-navigation__link" href="">Link</a>
+              <a style={{ color: '#616161' }} className="mdl-navigation__link" href="">{TAPi18n.__('header.link.interior')}</a>
+              <a style={{ color: '#616161' }} className="mdl-navigation__link" href="">{TAPi18n.__('header.link.outdoor')}</a>
+              <a style={{ color: '#616161' }} className="mdl-navigation__link" href="">{TAPi18n.__('header.link.artwork')}</a>
             </nav>
+            <div>
+              <img width="50px" height="50px" src="account.png" alt="Access Your Account" />
+            </div>
             <div
               className="mdl-textfield mdl-js-textfield mdl-textfield--expandable
                     mdl-textfield--floating-label mdl-textfield--align-right"
@@ -29,20 +32,20 @@ class Header extends Component {
               </label>
               <div className="mdl-textfield__expandable-holder">
                 <input
-                  className="mdl-textfield__input" type="text" name="sample"
+                  className="mdl-textfield__input" type="text"
                   id="waterfall-exp"
+                  placeholder="Green World"
                 />
               </div>
             </div>
           </div>
         </header>
         <div className="mdl-layout__drawer">
-          <span className="mdl-layout-title">Title</span>
+          <span className="mdl-layout-title">{TAPi18n.__('title.websitename')}</span>
           <nav className="mdl-navigation">
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">{TAPi18n.__('header.link.interior')}</a>
+            <a className="mdl-navigation__link" href="">{TAPi18n.__('header.link.outdoor')}</a>
+            <a className="mdl-navigation__link" href="">{TAPi18n.__('header.link.artwork')}</a>
           </nav>
         </div>
       </div>
