72df25986797062219a21dcd49638913778b0638
Chenxu Zhao <zhaochenxu001@gmail.com>
2017-02-06 21:08:36 +1100

    Add Animation in Sections.jsx

--- imports/ui/subpages/subhomepages/Sections.jsx
+++ imports/ui/subpages/subhomepages/Sections.jsx
@@ -7,21 +7,46 @@ class Sections extends Component {
       <div className="sections" id="sections">
         <Grid>
           <Cell col={4} tablet={8} phone={6}>
-            <img width="100%" src="plants/4.jpeg" alt="" />
+            <div className="container">
+              <img width="100%" src="plants/4.jpeg" alt="" className="image" />
+              <div className="overlay">
+                <div className="text">1</div>
+              </div>
+            </div>
           </Cell>
           <Cell col={8} tablet={8} phone={6}>
             <Grid>
               <Cell col={6} tablet={8} phone={6}>
-                <img style={{ maxWidth: '327px' }} width="100%" src="plants/5.jpg" alt="" />
+                <div className="container">
+                  <img style={{ maxWidth: '327px' }} width="100%" src="plants/5.jpg" alt="" />
+                  <div style={{ maxWidth: '350px' }} className="overlay">
+                    <div className="text">2</div>
+                  </div>
+                </div>
               </Cell>
               <Cell col={6} tablet={8} phone={6}>
-                <img style={{ maxWidth: '490px' }} width="100%" src="plants/1.jpg" alt="" />
+                <div className="container">
+                  <img style={{ maxWidth: '490px' }} width="100%" src="plants/1.jpg" alt="" />
+                  <div style={{ maxWidth: '490px' }} className="overlay">
+                    <div className="text">3</div>
+                  </div>
+                </div>
               </Cell>
               <Cell col={6} tablet={8} phone={6}>
-                <img style={{ maxWidth: '327px' }} width="100%" src="plants/2.jpeg" alt="" />
+                <div className="container">
+                  <img style={{ maxWidth: '327px' }} width="100%" src="plants/2.jpeg" alt="" />
+                  <div style={{ maxWidth: '350px' }} className="overlay">
+                    <div className="text">4</div>
+                  </div>
+                </div>
               </Cell>
               <Cell col={6} tablet={8} phone={6}>
-                <img style={{ maxWidth: '490px' }} width="100%" src="plants/3.jpg" alt="" />
+                <div className="container">
+                  <img style={{ maxWidth: '490px' }} width="100%" src="plants/3.jpg" alt="" />
+                  <div style={{ maxWidth: '490px' }} className="overlay">
+                    <div className="text">5</div>
+                  </div>
+                </div>
               </Cell>
             </Grid>
           </Cell>
